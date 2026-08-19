d92f833c0696283895ffc757c13d19dbad84fe07
archanaKurikuti <archanakurikuti567@gmail.com>
2026-08-19 20:32:13 +0000

    controller

---
+++ src/main/java/com/example/career_companion/controller/CandidateController.java
@@ -0,0 +1,34 @@
+package com.example.career_companion.controller;
+
+import com.example.career_companion.dto.candidate.CandidateResponse;
+import com.example.career_companion.dto.candidate.CandidateUpdateRequest;
+import com.example.career_companion.service.CandidateService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/candidates")
+public class CandidateController {
+
+    @Autowired
+    private CandidateService candidateService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CandidateResponse> getCandidate(
+            @PathVariable Long id) {
+
+        return ResponseEntity.ok(
+                candidateService.getCandidateById(id)
+        );
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<CandidateResponse> updateCandidate(
+            @PathVariable Long id,
+            @RequestBody CandidateUpdateRequest request) {
+
+        return ResponseEntity.ok(
+                candidateService.updateCandidate(id, request)
+        );
+    }
+}
